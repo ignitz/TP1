3889f452d7205c16d19870d014f1de3466f2c76e
ignitz <ignitzhjfk@gmail.com>
2014-04-14 10:29:42 -0300

    Update AGM.c

--- AGM.c
+++ AGM.c
@@ -14,8 +14,34 @@
 TipoGrafo ArvoreGMinima(TipoGrafo *Grafo)
 {
     TipoGrafo A;
+
     A.NumVertices = Grafo->NumVertices;
-    A.NumArestas = 0;
+    FGVazio(&A);
+
+    TipoApontador celAux;
+    celAux = Grafo->Adj[0].Primeiro;
+
+    TipoValorVertice iTemp = 0xFFFFFFFF;
+    int iIndex = 1;
+
+    while(celAux->Prox != NULL)
+    {
+        /* find the minumum cost */
+        if(!(ExisteAresta(iIndex, celAux->Item.Vertice, &A)))
+        {
+            if(celAux->Item.Peso < iTemp)
+                iTemp = celAux->Item.Peso;
+        }
+        celAux = celAux->Prox; ++iIndex;
+    }
+
+    int V1,
+        V2;
+        V1 = 0;
+        V2 = iIndex-1;
+
+
+    InsereAresta(&V1, &V2, &iTemp, &A);
 
     return A;
 }
